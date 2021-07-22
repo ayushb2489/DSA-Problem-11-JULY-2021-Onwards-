62757f594ff1a7c1eb0c690acade74ec4f1d0292
unknown <ayushb2489@gmail.com>
2021-07-22 22:29:01 +0530

    22 July

---
+++ minOperationstring.cpp
@@ -0,0 +1,20 @@
+	int minOperations(string s, string t) 
+	{ 
+	    int n = s.size();
+		    int m = t.size();
+		    int** dp = new int*[n + 1];
+		    for (int i = 0; i <= n; i++)
+		    {
+		        dp[i] = new int[m + 1];
+		        for (int j = 0; j <= m; j++)
+		        {
+		            if (i == 0 || j == 0)
+		                dp[i][j] = 0;
+		            else if (s[i - 1] == t[j - 1])
+		                dp[i][j] = 1 + dp[i - 1][j - 1];
+		            else
+		                dp[i][j] = max (dp[i - 1][j], dp[i][j - 1]);
+		        }
+		    }
+		    return n + m - 2 * dp[n][m];
+	} 
